0e861f81ad6c3a8f5575ad9558d0ef3a64732727
Raj Joshi <geeky2307@gmail.com>
2018-10-27 13:50:23 -0400

    added amazon_scrapper folder + minified amazon_scrapper.py

---
+++ amazon_scraper/amazon_scraper.min.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import random
+class AmazonProduct: def __init__(self, address): self.price = ""
+self.model_number = ""
+self.address = address
+self.entry_list = []
+self.headers = [{'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; LG-H345 Build/LMY47V) AppleWebKit/537.36 ' + '(KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36 OPR/30.0.1856.93524'}, ]
+self.req = urllib.request.Request(self.address, data = None, headers = self.headers[(random.randint(0, len(self.headers) - 1))])
+self.data = urllib.request.urlopen(self.address).read()
+def retrieve_item_model(self): try: soup = BeautifulSoup(self.data, "lxml")
+for number in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]: for row in soup.find_all(id = 'productDetails_techSpec_section_' + str(number)): for tr in row.find_all('tr'): self.entry_list.append(tr.text.strip())
+for row in soup.find_all(id = 'productDetails_detailBullets_sections1'): for tr in row.find_all('tr'): self.entry_list.append(tr.text.strip())
+for x in self.entry_list: if "Item model number" in x: self.model_number = x[17: ].strip()
+except AttributeError: self.model_number = None
+except requests.HTTPError as e: print("From Amazon", e)
+def retrieve_item_price(self): if self.model_number is None: return
+soup = BeautifulSoup(self.data, "lxml")
+self.price = soup.find(id = 'priceblock_ourprice').text.strip()
+return
