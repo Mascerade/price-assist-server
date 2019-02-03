0d22fc02e63a69bbb916c075d4cb5cf19ff2259f
BinaryWiz <Ultimi450@gmail.com>
2019-02-03 12:24:02 -0500

    made master_scraper more verbose

--- master_scraper/master_scraper.py
+++ master_scraper/master_scraper.py
@@ -1,14 +1,18 @@
-class Scraper():
-    def __init__(self, search_address, product_model, user_agent, data):
+import os
+import json
+
+
+class Scraper:
+    file = open(os.getcwd() + "/adjs.json", "r")
+    adjectives = json.load(file)
+    file.close()
+
+    def __init__(self, search_address, product_model, user_agent, title, data, amazon_title=""):
         self.price = ""
         self.search_address = search_address
         self.product_address = ""
         self.product_model = product_model
         self.user_agent = user_agent
+        self.title = title
+        self.amazon_title = amazon_title
         self.data = data
-
-
-class SubScraper(Scraper):
-    def __init__(self, product_model):
-        super().__init__("search_address", product_model, "user_agent", "data")
-
--- newegg_scraper/newegg_scraper.py
+++ newegg_scraper/newegg_scraper.py
@@ -8,24 +8,39 @@ from master_scraper.master_scraper import Scraper
 
 
 class NeweggProduct(Scraper):
-    def __init__(self, product_model):
-        self.price = ""
-        self.product_address = ""
+    def __init__(self, product_model, amazon_title):
         with open(os.getcwd() + "\\user_agents\\newegg_agents.txt", "r") as scrapers:
             headers = {"User-Agent": random.choice(scrapers.read().splitlines())}
+
+        # Initializes the Scraper class with the NeweggProduct
         super().__init__(search_address='https://www.newegg.com/Product/ProductList.aspx?' +\
                                       'Submit=ENE&DEPA=0&Order=BESTMATCH&Description={}'\
                                 .format(product_model),
                          product_model=product_model,
+                         title=None,
                          user_agent=headers,
-                         data="")
+                         data="",
+                         amazon_title=amazon_title)
+        self.prices = []
+        self.titles_links = None
         self.data = requests.get(self.search_address, headers=self.user_agent)
         self.data = self.data.text
         self.soup = BeautifulSoup(self.data, 'html.parser')
 
-    def retrieve_product_address(self):
+    def retrieve_product_information(self):
         try:
-            self.product_address = self.soup.find("a", attrs={"class": "item-title", "title": "View Details"})['href']
+            # Includes info about link AND actual title
+            full_titles = self.soup.find_all("a", attrs={"class": "item-title", "title": "View Details"})[:6]
+            titles = []
+            links = []
+
+            for index, title in enumerate(full_titles):
+                titles.append(title.text)  # Appends the actual title of the product
+                links.append(title['href'])  # Appends just the link to the product page of the product
+
+                self.prices.append(self.check_price(self.soup.findAll("li", attrs={"class": "price-current"})[index].text))
+
+            self.titles_links = dict(zip(titles, links))  # Makes the title name the key and the link the value in a dict
 
         except AttributeError:
             self.product_address = "None"
@@ -36,33 +51,49 @@ class NeweggProduct(Scraper):
         except Exception as e:
             self.product_address = "None"
 
-    def retrieve_product_price(self):
+    def get_best_product(self):
+        """
+
+        """
+
+    def check_price(self, price):
         try:
-            self.price = "$"
+            self.adjusted_price = ""
             stop = False
-            for x in self.soup.find("li", "price-current").text:
+            for x in price:
                 if not stop:
                     if x.isdigit():
-                        self.price += x
+                        self.adjusted_price += x
 
                     elif x == "(":
                         stop = True
 
-                    elif x == "." or x == ",":
-                        self.price += x
+                    elif x == ".":
+                        self.adjusted_price += x
 
-            if self.price.strip(" ") == "$":
-                self.price = "Price Shown In Cart"
+            if self.adjusted_price.strip(" ") == "":
+                self.adjusted_price = None
 
             else:
-                self.price = self.price.strip(" ")
+                self.adjusted_price = self.adjusted_price.strip(" ")
+                return self.adjusted_price
 
         except AttributeError as e:
-            self.price = "Could Not Find Price"
+            self.adjusted_price = None
 
         except TypeError as e:
-            self.price = "Could Not Find Price"
+            self.adjusted_price = None
 
         except Exception as e:
-            self.price = "Could Not Find Price"
+            self.adjusted_price = None
+
+    def print_titles_links(self, dict):
+        for key, value in dict.items():
+            print(key, ": ", value)
+
+
+newegg = NeweggProduct("789564-0010",
+                       'Bose QuietComfort 35 (Series II) Wireless Headphones, Noise Cancelling, with Alexa voice control - Black')
+newegg.retrieve_product_information()
+newegg.get_best_product()
 
