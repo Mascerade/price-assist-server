c5025e2fab41e8c18c19710490a1ff10cb347b49
BinaryWiz <Ultimi450@gmail.com>
2019-02-16 21:56:27 -0500

    got best buy scraper to work

--- bestbuy_scraper/bestbuy_scraper.py
+++ bestbuy_scraper/bestbuy_scraper.py
@@ -1,25 +1,28 @@
 from bs4 import BeautifulSoup
-import urllib.request
+import random
+import requests
+import os
+import sys
+sys.path.append(os.getcwd())
+from master_scraper.master_scraper import Scraper
 
 
-class BestBuy:
+class BestBuy(Scraper):
     def __init__(self, product_model):
-        self.price = ""
-        self.product_model = product_model
-        self.product_search_address = 'https://www.bestbuy.com/site/searchpage.jsp?st={}'.format(product_model)
-        print(self.product_search_address)
-        self.product_address = "None"
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; rv:52.0) Gecko/20100101 Firefox/52.0'
-        }
-        data = urllib.request.Request(self.product_search_address, headers=self.headers)
-        data = urllib.request.urlopen(data).read()
-        self.soup = BeautifulSoup(data, "html.parser")
+        with open(os.getcwd() + "\\user_agents\\walmart_agents.txt", "r") as scrapers:
+            headers = {"User-Agent": random.choice(scrapers.read().splitlines())}
+        super().__init__(name="BestBuy",
+                         search_address='https://www.bestbuy.com/site/searchpage.jsp?st={}'.format(product_model),
+                         product_model=product_model,
+                         user_agent=headers,
+                         data="")
+        data = requests.get(self.search_address, headers=headers).text
+        self.soup = BeautifulSoup(data, "lxml")
 
     def retrieve_product_address(self):
         try:
             sku_header = self.soup.find('h4', 'sku-header')
-            self.product_address = sku_header.find('a')['href']
+            self.product_address = "https:www.bestbuy.com" + sku_header.find('a')['href']
 
         except AttributeError:
             self.product_address = "None"
@@ -28,8 +31,14 @@ class BestBuy:
             self.product_address = "None"
 
     def retrieve_product_price(self):
-        if self.product_search_address is not None:
-            self.price = self.soup.find('div', 'priceView-hero-price priceView-purchase-price').text
+        if self.product_address is not None:
+            self.price = self.soup.find('div', 'priceView-hero-price priceView-customer-price').find("span").text
 
         else:
             self.price = "Could Not Find Price"
+
+
+best = BestBuy("220-G3-0750-X1")
+best.retrieve_product_address()
+best.retrieve_product_price()
+print(best.product_address, best.price)
