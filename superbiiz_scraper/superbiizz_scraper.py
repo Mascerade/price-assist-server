54b51b033e1122bda980a377ddd9079ea90dd283
BinaryWiz <Ultimi450@gmail.com>
2019-03-02 15:42:19 -0500

    add new_scraper template for easier creation of new scrapers. Also added superbiiz as a scraper

---
+++ superbiiz_scraper/superbiizz_scraper.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import random
+import os
+import sys
+import requests
+sys.path.append(os.getcwd())
+from master_scraper.master_scraper import Scraper
+
+
+class SuperBlitz(Scraper):
+    def __init__(self, product_model):
+        with open(os.path.join(os.getcwd(), 'user_agents', 'scrapers_master.txt'), "r") as scrapers:
+            header = {"User-Agent": random.choice(scrapers.read().splitlines())}
+
+        super().__init__(name="Super Blitz",
+                         search_address='https://www.superbiiz.com/query.php?s={}'.format(product_model),
+                         product_model=product_model,
+                         user_agent=header,
+                         data="")
+        try:
+            data = requests.get(self.search_address, headers=self.user_agent).text
+            self.soup = BeautifulSoup(data, Scraper.parser)
+
+        except Exception as e:
+            self.price = "Could not find price"
+            self.product_address = "None"
+
+    def retrieve_product_price(self):
+        try:
+            # for index, x in enumerate(self.soup.find_all("tr")):
+            #     print(x.text, index)
+            self.price = self.soup.find_all("td", attrs={"width": "1%"})[0].find("a").text.strip()
+            new_price = ""
+            for char in self.price:
+                if char == "$" or char == "." or char.isdigit():
+                    new_price += char
+            self.price = new_price
+
+        except AttributeError as e:
+            self.price = "Could not find price"
+
+        except Exception as e:
+            self.product_address = "None"
+
+    def retrieve_product_address(self):
+        try:
+            self.product_address = "https:www.superbiiz.com" + self.soup.find_all("tr")[45].find("a")["href"]
+
+        except AttributeError as e:
+            self.product_address = "None"
+
+        except Exception as e:
+            self.product_address = "None"
+
+
+superbiiz = SuperBlitz("i7 8700")
+superbiiz.retrieve_product_address()
+superbiiz.retrieve_product_price()
+print(superbiiz.price, superbiiz.product_address)
