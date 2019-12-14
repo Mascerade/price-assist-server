99ad9196f4bc6a5acaf8f126e94c3da483d4c68e
BinaryWiz <Ultimi450@gmail.com>
2019-12-14 13:01:07 -0500

    more documentation

--- track_prices/history_manager.py
+++ track_prices/history_manager.py
@@ -162,10 +162,13 @@ def put_data():
         cursor.close()
         conn.close()
 
+        # Open a connection the the plyvel database
         ply_db = plyvel.DB('item_model_db/', create_if_missing = True)
 
+        # Put the item_model into the database/do nothing if its already there
         ply_db.put(bytes(item_model, encoding='utf-8'), bytes(True))
         
+        # Close the plyvel database
         ply_db.close()
 
         return json.dumps({"success": True}), 204
