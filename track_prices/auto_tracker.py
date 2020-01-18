e244d0c3cc79d590108e25938eb4b340c018defb
BinaryWiz <Ultimi450@gmail.com>
2020-01-18 18:24:03 -0500

    got rid of the gui only

---
+++ track_prices/auto_tracker.py
@@ -0,0 +1,13 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+
+def price_updater():
+    data = requests.get("http://localhost:5003/item_model_data").json()
+    for item_model, _ in data.items():
+        data = requests.get("http://localhost:5000/api/query?retailer=None&price=None&item_model=" + item_model + "&return_type=json").json()
+        requests.put("http://localhost:5003/", json = {"item_model": item_model, "data": data})
+
+
+sched = BlockingScheduler()
+sched.add_job(price_updater, trigger='cron', hour='15', minute='00')
+sched.start()
---
+++ track_prices/test_script.py
@@ -0,0 +1,24 @@
+import requests
+
+while True:
+    user_input = input("Do you want to GET, PUT or DELETE? ")
+
+    if user_input.lower() == "get":
+        pass
+
+    elif user_input.lower() == "put":
+        data = requests.get("http://localhost:5000/api/query?retailer=None&price=$471.99&item_model=bx80684i99900k&return_type=json").json()
+        requests.put("http://localhost:5003/", json = {"item_model": "bx80684i99900k", "data": data})
+        requests.put("http://localhost:5003/item_model_data", json={"item_model": data["item_model"], "title": data["title"]})
+
+    elif user_input.lower() == "delete":
+        item_model = 'bx80684i99900k'
+        requests.delete("http://localhost:5003/", json = {"item_model": item_model})
+
+    elif user_input.lower() == "put item model":
+        item_model = input("What item model do you want to put into the database? ")
+        requests.put("http://localhost:5003/item_model_data", json={"item_model": item_model, "title": "test"})
+
+    elif user_input.lower() == "delete item model":
+        item_model = input("What item model do you want to put into the database? ")
+        requests.delete("http://localhost:5003/item_model_data", json={"item_model": item_model})
