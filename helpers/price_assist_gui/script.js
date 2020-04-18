2b5851155eb797fd0563650d20987dff2e14cd8d
BinaryWiz <Ultimi450@gmail.com>
2020-04-18 16:31:25 -0400

    GOT GUI WORKING WHEN INSERTED

--- helpers/price_assist_gui/script.js
+++ helpers/price_assist_gui/script.js
@@ -2,9 +2,6 @@ var app = new Vue({
   el: '#content',
   data: {
     retailerData: [
-      {'title': 'Amazon', 'price': 489.99, 'link': 'http://www.timeless-apps.com/'},
-      {'title': 'Newegg', 'price': 509.99, 'link': 'http://www.timeless-apps.com/'},
-      {'title': 'Walmart', 'price': 519.99, 'link': 'http://www.timeless-apps.com/'}
     ]
   },
 
