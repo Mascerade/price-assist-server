a700a5f6f4d2190732e3c9e53236b297accfa9c4
BinaryWiz <ultimi450@gmail.com>
2020-06-05 13:02:28 -0400

    the tokens can now be generated and are unique

--- user_database_controller/database_controller.py
+++ user_database_controller/database_controller.py
@@ -6,6 +6,11 @@ import requests
 import sys
 import os
 import plyvel
+import firebase_admin
+from firebase_admin import auth
+
+# Initialize the firebase app
+default_app = firebase_admin.initialize_app()
 
 # Create the databases directory first
 if not os.path.exists('databases'):
@@ -18,13 +23,24 @@ ply_db = plyvel.DB(USER_DB, create_if_missing = True)
 
 app = Flask(__name__)
 
+@app.route('/test', methods=['GET'])
+@cross_origin()
+def test():
+    token = request.args.get('uid_token')
+    decoded_token = auth.verify_id_token(token)
+    uid = decoded_token['uid']
+    print(uid)
+    return json.dumps({'success': True}), 200
+
 @app.route('/', methods=['POST'])
+@cross_origin()
 def add_user():
     uid_token = request.json['uid_token']
     ply_db.put(bytes(uid_token, encoding='utf-8'), bytes(json.dumps({'item_models': []}), encoding='utf-8'))
     return json.dumps({'success': True, 'msg': 'User Created'}), 201
 
 @app.route('/', methods=['PUT'])
+@cross_origin()
 def update_item_models():
     uid_token = request.json['uid_token']
     item_model = request.json['item_model']
@@ -37,6 +53,7 @@ def update_item_models():
     return json.dumps({'success': True, 'msg': 'Updated Item Models'}), 204
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def get_user():
     uid_token = request.args.get('uid_token')
     item_models = ply_db.get(bytes(uid_token, encoding='utf-8'))
