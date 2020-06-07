5415e07409ae639d627156cc4982d666f78f2148
BinaryWiz <Ultimi450@gmail.com>
2020-06-07 13:35:28 -0400

    added ability to delete users and item models from users

--- user_database_controller/database_controller.py
+++ user_database_controller/database_controller.py
@@ -67,5 +67,31 @@ def get_user():
     item_models['success'] = True
     return json.dumps(item_models), 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, threaded=True)
+@app.route('/', methods=['DELETE'])
+@cross_origin()
+def delete_user():
+    uid_token = process_token(request.json['uid_token'])
+    result = ply_db.get(bytes(uid_token, encoding='utf-8'))
+    if result is None:
+        return json.dumps({'success': False, 'msg': 'User does not exist'}), 404
+    ply_db.delete(bytes(uid_token, encoding='utf-8'))
+    return json.dumps({'success': True, 'msg': 'User deleted'}), 204
+
+@app.route('/del_item_model', methods=['DELETE'])
+@cross_origin()
+def del_item_model():
+    uid_token = process_token(request.json['uid_token'])
+    target_item_model = request.json['item_model']
+    user_item_models = ply_db.get(bytes(uid_token, encoding='utf-8'))
+    if user_item_models is None:
+        return json.dumps({'success': False, 'msg': 'User does not exist'}), 404
+    user_item_models = json.loads(user_item_models.decode('utf-8'))
+    try:
+        user_item_models['item_models'].remove(target_item_model)
+        ply_db.put(bytes(uid_token, encoding='utf-8'), bytes(json.dumps(user_item_models), encoding='utf-8'))
+        return json.dumps({'success': True, 'msg': 'Removed the item model'}), 204
+    except ValueError:
+        return json.dumps({'success': False, 'msg': 'Item model never existed'}), 404
+            
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002, threaded=True)
--- user_database_controller/helpers/test_script.py
+++ user_database_controller/helpers/test_script.py
@@ -1,16 +1,25 @@
 import requests
 
 while True:
-    user_input = input("Do you want to GET, PUT or DELETE? ")
+    user_input = input('Do you want to GET, PUT or DELETE? ')
 
-    if user_input.lower() == "get":
+    if user_input.lower() == 'get':
         pass
 
-    elif user_input.lower() == "post":
+    elif user_input.lower() == 'post':
         uid_token = input('What do you want the uid token to be? ')
-        requests.post("http://localhost:5002/", json = {"uid_token": uid_token})
+        requests.post('http://localhost:5002/', json = {'uid_token': uid_token})
 
-    elif user_input.lower() == "put":
-        uid_token = input("What user do you want to use? ")
-        item_model = input("What item model do you want to put? ")
-        requests.put("http://localhost:5002/", json={"uid_token": uid_token, "item_model": item_model})
+    elif user_input.lower() == 'put':
+        uid_token = input('What user do you want to use? ')
+        item_model = input('What item model do you want to put? ')
+        requests.put('http://localhost:5002/', json={'uid_token': uid_token, 'item_model': item_model})
+
+    elif user_input.lower() == 'delete':
+        user = input('What user would you like to delete? ')
+        requests.delete('http://localhost:5002', json={'uid_token': user})
+
+    elif user_input.lower() == 'delete item model':
+        uid_token = input('What user do you want to use? ')
+        item_model = input('What item model would you like to delete? ')
+        requests.delete('http://localhost:5002/del_item_model', json={'uid_token': uid_token, 'item_model': item_model})
